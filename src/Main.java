2732bab18f5acec8bb7dcfc6814e0259b3aeae6f
Leno4ek86 <102864231+Leno4ek86@users.noreply.github.com>
2022-04-25 17:56:03 +0300

    добавлен расчет миллей

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int around =17000; // стоимость билета
+        int mille =20; // рублей за 1 миллю
+
+        System.out.println((around / mille + "начислено миллей"));
+
+    }
+
+
+}
